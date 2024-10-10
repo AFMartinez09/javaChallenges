46d8ae04a4bbba720dae5a1e6a56836509c7145f
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-10-10 14:10:46 -0500

    finishing  Sales System

---
+++ sales_system/Sales.java
@@ -0,0 +1,15 @@
+package sales_system;
+
+public class Sales {
+  public static void main(String[] args){
+    System.out.println("*** Sales System ***");
+    var product1 = new Products("shorts", 20.33);
+    var product2 = new Products("shoes", 80.99);
+
+    var order1 = new Order();
+    order1.addProduct(product1);
+    order1.addProduct(product2);
+    order1.showOrder();
+
+  }
+}
